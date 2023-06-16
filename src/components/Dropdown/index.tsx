09ae634f3badfb0ad5345f9884e30a5e97ca6374
Yasmin Hernandes <yasminhernandesjobs@gmail.com>
2023-06-15 22:11:27 -0300

    feat: create dropdown in products page

---
+++ src/components/Dropdown/index.tsx
@@ -0,0 +1,66 @@
+
+import arrowDown from 'assets/svg/arrow-down.svg'
+
+import './style.scss'
+import { useEffect, useRef, useState } from 'react'
+
+type Props = {
+  placeholder: string,
+  options: [
+    {value: string, label: string}
+  ]
+}
+
+
+export const Dropdown = ({ placeholder, options }: Props) => {
+  const [showMenu, setShowMenu] = useState(false)
+  const inputRef = useRef<HTMLDivElement | null>(null)
+  const [selectedValue, setSelectedValue] = useState<string>('')
+
+  const getTitle = () => {
+   if(!selectedValue || selectedValue === null) return placeholder;
+    return selectedValue
+  } 
+
+  const handleClick = () => {
+    setShowMenu(!showMenu)
+  }
+
+  const handleOptionClick = (e: any) => {
+    setSelectedValue(e.target.innerHTML)
+  }
+
+  useEffect(() => {
+    const handler = (e : any) => {
+      if (inputRef.current && !inputRef.current.contains(e.target)) {
+        setShowMenu(false);
+      }
+    };
+
+    window.addEventListener("click", handler);
+    return () => {
+      window.removeEventListener("click", handler);
+    };
+  });
+
+
+  return (
+    <div className="dropdown-container" onClick={handleClick}>
+      <div ref={inputRef} className="dropdown-container__input">
+        {getTitle()}
+        <img src={arrowDown} alt="arrow down" className="select__arrow" />
+      </div>
+      { showMenu && (
+        <div className="dropdown-container__options">
+          {options.map(option => (
+            <div key={option.value} 
+                onClick={handleOptionClick}
+                className="dropdown-option">
+              {option.label}
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  )
+}
---
+++ src/types/priority-types.ts
@@ -0,0 +1,7 @@
+export enum PriorityTypes {
+  'RELEVANCY',
+  'HIGHEST_TO_LOWEST',
+  'LOWEST_TO_HIGHEST',
+  'MOST_RATED',
+  'BEST_SELLERS'
+}
