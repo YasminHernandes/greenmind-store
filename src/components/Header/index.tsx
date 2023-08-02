5edd0019eda0755b532c00fc5315d3897a18a9b6
Yasmin Hernandes <yasminhernandesjobs@gmail.com>
2023-08-02 20:19:47 -0300

    fix: add animation on minicart and menu mobile

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -22,7 +22,7 @@ export const Header = () => {
       e.key === 'Escape' && setMenuOpen(false)
     }),
     document.body.style.overflow = 'hidden'
-  ) : document.body.style.overflow = 'auto'
+  ) : document.body.style.overflowY = 'auto'
 
 
   useEffect(() => {
