29917dcb85bc5febcc73050897965fe096d98794
Yasmin Hernandes <yasminhernandesjobs@gmail.com>
2023-08-04 14:33:20 -0300

    fix(minicart context): import context and config vite-env.d.ts

--- src/hooks/useMinicartContext.ts
+++ src/hooks/useMinicartContext.ts
@@ -1,5 +1,5 @@
 import { useContext } from "react"
-import { MinicartContext } from "@/contexts/MinicartContext"
+import { MinicartContext } from "@/contexts"
 
 export const useMinicartContext = () => {
   return useContext<any>(MinicartContext)
